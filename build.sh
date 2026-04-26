68a28a5e34225930ce57adcfbbd7e10f7a0e0c7f
raswin <raswin@example.com>
2026-04-26 22:31:43 +0530

    fix: switch vercel to lightweight frontend and improve build script

--- build.sh
+++ build.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
-# Install Flutter SDK
-if [ ! -d "flutter" ]; then
+# Use existing flutter if available to save time/resources
+if command -v flutter &> /dev/null; then
+  echo "Using system flutter"
+elif [ -d "/home/raswin/development/flutter/bin" ]; then
+  export PATH="$PATH:/home/raswin/development/flutter/bin"
+  echo "Using development flutter"
+elif [ -d "flutter" ]; then
+  export PATH="$PATH:`pwd`/flutter/bin"
+  echo "Using local flutter"
+else
+  echo "Cloning Flutter SDK..."
   git clone https://github.com/flutter/flutter.git -b stable --depth 1
+  export PATH="$PATH:`pwd`/flutter/bin"
 fi
-export PATH="$PATH:`pwd`/flutter/bin"
+
 flutter config --enable-web
 cd flutter_app
+flutter pub get
 flutter build web --release
